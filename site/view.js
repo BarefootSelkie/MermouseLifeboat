c0fd7c40388b28c67b99817736dbc5f94cbd244a
Aithne Morgana <MxAithne@users.noreply.github.com>
2019-10-30 13:40:57 +0000

    Uses partial templates, added pagination

--- site/view.js
+++ site/view.js
@@ -12,13 +12,27 @@ function loadTemplate(name) {
   });
 }
 
+// Wrapper function to make promises work better with jquery
+function loadPartial(name) {
+  return new Promise(function(resolve, reject) {
+    $.get("handlebars/" + name + ".handlebars",(data) => {
+      Handlebars.registerPartial(name, data);
+    })
+      .done(resolve)
+      .fail(reject);
+  });
+}
+
 // Load in templates from .handlebars files
 function loadTemplates()
 {
   return new Promise((resolve, reject) => {
     Promise.all([
-      loadTemplate("stationFull"),
-      loadTemplate("stationSummary")
+      loadTemplate("pageIndex"),
+      loadTemplate("pageOverview"),
+      loadPartial("itemOverview"),
+      loadPartial("itemStationSummary"),
+      loadPartial("paginator")
     ])
     .then(resolve);
   });
--- site/viewmodel.js
+++ site/viewmodel.js
@@ -1,3 +1,46 @@
+
+var page = 0;
+var pageSize = 8;
+
+// Custom sort function for sorting by date property
+var sortByDate = (a, b) => {
+  if (a.date > b.date) { return 1; }
+  if (a.date < b.date) { return -1; }
+  return 0;
+};
+
+function makeSiteViewModel()
+{
+  var stationCount = lifeboatStationLocations.length;
+  var visitedCount = visitedStations.filter((station) => station.Visits.length > 0).length;
+
+  return {
+    "stations": lifeboatStationLocations
+                    .map((station) => makeStationViewModel(station.SAP_ID))
+                    .sort((a, b) => { // sort alphabetically by name
+                      if (a.name < b.name) { return -1; }
+                      if (a.name > b.name) { return 1; }
+                      return 0;
+                    })
+                    .sort((a, b) => { // sort by visited stations first
+                      if (statusOrder.indexOf(a.status) < statusOrder.indexOf(b.status)) { return -1; }
+                      if (statusOrder.indexOf(a.status) > statusOrder.indexOf(b.status)) { return 1; }
+                      return 0;
+                    })
+    ,
+    "visitedStations": visitedStations
+                        .map((station) => makeStationViewModel(station.SAP_ID))
+                        .sort(sortByDate)
+                        .reverse()
+                        .filter((station,i) => i >= (page * pageSize) && i < Math.min((page + 1) * pageSize, visitedCount))
+    ,
+    "visitedPercent": Math.round(100 * visitedCount / stationCount),
+    "currentPage": page,
+    "previousPage": Math.max(page-1,0),
+    "nextPage": Math.min(page+1,Math.ceil(visitedCount / pageSize))
+  }
+}
+
 function makeStationViewModel(stationID) {
 
   // Get information about this location
@@ -27,8 +70,8 @@ function makeStationViewModel(stationID) {
     status: visitInformation ? !visitInformation.Revisit ? '💚' : '🔶' : '🔴',
     pin: visitInformation ? pinBadges[visitInformation.Pin] : '',
     visits: visits,
-    lastVisit: visits.length > 0 ? visits[0] : null,
-    date: visits.length > 0 ? visits[0].date : "",
+    lastVisit: visits.length > 0 ? visits[visits.length - 1] : null,
+    date: visits.length > 0 ? visits[visits.length - 1].date : "",
   };
 }
 
